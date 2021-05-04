510ef24e3604137a9eddfa740d6001d11ffb176e
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-05-04 14:58:18 -0400

    deleted react default app and added a header to the page with no styling

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+            <p>Click on the carrots to filter by heading or use the search box to narrow your results</p>
+        </div>
+    )
+}
+
+export default Header;
