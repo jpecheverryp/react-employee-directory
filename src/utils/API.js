aafe7ba5ec3ffc23168439ea38b9f4fd6ef8644f
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-05-05 01:05:54 -0400

    added filter on api call to get only the neccesary data

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,7 +4,23 @@ export default {
     getEmployees: async function() {
         try {
             const response = await axios.get("https://randomuser.me/api/?results=20")
-            return response.data.results
+            const apiInfo = response.data.results;
+            const myObject = apiInfo.map(({
+                picture,
+                name,
+                phone,
+                email,
+                dob
+            }) => {
+                return {
+                    picture,
+                    name,
+                    phone,
+                    email,
+                    dob
+                }
+            })
+            return myObject
         } catch (err) {
             console.error(err)
         }
